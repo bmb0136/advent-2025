05018cc5862c55189daefde4e9892b4e9a26fde2
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-01 23:13:15 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        systems = [
+          "x86_64-linux"
+        ];
+
+        imports = [
+          inputs.treefmt-nix.flakeModule
+        ];
+
+        perSystem =
+          { ... }:
+          {
+            treefmt = import ./treefmt.nix;
+          };
+      }
+    );
+}
