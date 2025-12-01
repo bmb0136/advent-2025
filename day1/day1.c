013878823c5e4d957ad3f1c56de162826bdd26ec
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-01 23:43:47 +0000

    Do day 1

--- day1/day1.c
+++ day1/day1.c
@@ -2,5 +2,48 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    puts("Usage: day1 <input file>");
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+  if (!file) {
+    fprintf(stderr, "Failed to open file %s\n", argv[1]);
+    return 1;
+  }
+
+  // Position starts at 50
+  int position = 50;
+
+  // Number of zeroes
+  int answer = 0;
+
+  char line[10];
+  while (!feof(file)) {
+    if (fgets(line, 10, file) != line) {
+      continue;
+    }
+
+    char direction = line[0];
+    int amount = atoi(line + 1);
+
+    if (direction == 'L') {
+      position -= amount;
+    } else {
+      position += amount;
+    }
+
+    // Wrap around (extra mod handles negatives safely)
+    position = ((position % 100) + 100) % 100;
+
+    if (position == 0) {
+      answer++;
+    }
+  }
+
+  printf("Answer: %d\n", answer);
+
+  fclose(file);
   return 0;
 }
