bb3b9fe15692702503d6f3eedd36266aed29cd7d
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-04 03:25:50 +0000

    Add day 3 files

---
+++ day3a/default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs ? import <nixpkgs> {},
+  buildGoModule ? pkgs.buildGoModule,
+  ...
+}: buildGoModule {
+  name = "day3a";
+  src = ./.;
+  vendorHash = null;
+  postInstall = ''
+    mv $out/bin/main $out/bin/day3a
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
             packages.day1b = pkgs.callPackage ./day1b { };
             packages.day2a = pkgs.callPackage ./day2a { };
             packages.day2b = pkgs.callPackage ./day2b { };
+            packages.day3a = pkgs.callPackage ./day3a { };
           };
       }
     );
