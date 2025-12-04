19e58a750a2c39b68d07b52abc54b58c47b5c344
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-04 03:51:23 +0000

    Do day 3 part 1

--- day3a/main.go
+++ day3a/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	_ "embed"
+)
+
+//go:embed input.txt
+var input string
 
 func main() {
-	fmt.Println("Hello, World")
+	banks := strings.Split(input, "\n")
+
+	total := 0
+
+	for _, line := range banks {
+		if len(line) == 0 {
+			continue
+		}
+
+		batteries := make([]int, len(line))
+		for i, c := range line {
+			batteries[i] = int(c) - 0x30
+		}
+
+		max_jolt := 0
+		max_val := 0
+		for i, d := range batteries {
+			if d > max_val && i != len(batteries) - 1 {
+				max_val = d
+				max_jolt = 0
+			} else {
+				max_jolt = max(max_jolt, (10 * max_val) + d)
+			}
+		}
+
+		total += max_jolt
+	}
+
+	fmt.Printf("Answer: %d\n", total)
 }
