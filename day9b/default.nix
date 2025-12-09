ee0fa58ed9829b37789a3e8fe77b21c9c4b6693f
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-09 22:46:12 +0000

    Add day 9 part 2 files

---
+++ day9b/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+pkgs.stdenv.mkDerivation rec {
+  name = "day9b";
+  src = ./.;
+
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+    (pkgs.haskellPackages.ghcWithPackages (p: with p; [split text]))
+  ];
+
+  buildPhase = ''
+    ghc main.hs
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share
+    mkdir -p $out/bin
+
+    cp input.txt $out/share/
+    cp main $out/bin/
+
+    makeWrapper $out/bin/main $out/bin/${name} \
+      --add-flags "$out/share/input.txt"
+  '';
+}
